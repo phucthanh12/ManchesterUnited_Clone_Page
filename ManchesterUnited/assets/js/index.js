9989eabcaba55952abe2defd7b86abb08fec57b5
phucthanh12 <73888630+phucthanh12@users.noreply.github.com>
2022-06-04 23:20:17 +0700

    try new things

--- ManchesterUnited/assets/js/index.js
+++ ManchesterUnited/assets/js/index.js
@@ -1,15 +1,25 @@
 window.onload = function(){
     const overlay = document.querySelector(".overlay");
-    const modalContainer = document.querySelector(".content-modal__container");
+    const modalContainers = document.querySelectorAll(".content-modal__container");
+    const contentTitles = document.querySelectorAll(".content-title");
 
-    modalContainer.addEventListener("mouseover",event =>{
-        overlay.style.display=" block";
-        overlay.style.opacity=" 0.4";
-    });
+    modalContainers.forEach(modalContainer => modalContainer.addEventListener("mouseover", () =>{
+        overlay.style.display="block";
+        overlay.style.opacity="0.3";
+    }));
+  
+    contentTitles.forEach(contentTitle => contentTitle.addEventListener("mouseover", () =>{
+        overlay.style.display="block";
+        overlay.style.opacity="0.3";
+    }));
+  
+    modalContainers.forEach(modalContainer => modalContainer.addEventListener("mouseout",()=>{
+        overlay.style.display="none";
+        overlay.style.opacity="0";
+    }));
 
-    modalContainer.addEventListener("mouseout",event =>{
-        // event.preventDefault()
-        overlay.style.display=" none";
-        overlay.style.opacity=" 0"; 
-    });
+    contentTitles.forEach(contentTitle => contentTitle.addEventListener("mouseout",()=>{
+        overlay.style.display="none";
+        overlay.style.opacity="0";
+    }));
 }
