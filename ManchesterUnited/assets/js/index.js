b2ea0b9a1b7aeaec2f41c4f8c7f1227039123f7c
phucthanh12 <73888630+phucthanh12@users.noreply.github.com>
2022-06-06 15:11:19 +0700

    commit

--- ManchesterUnited/assets/js/index.js
+++ ManchesterUnited/assets/js/index.js
@@ -1,25 +1,50 @@
 window.onload = function(){
-    const overlay = document.querySelector(".overlay");
     const modalContainers = document.querySelectorAll(".content-modal__container");
-    const contentTitles = document.querySelectorAll(".content-title");
+    const headers = document.querySelectorAll(".nav-content__title");
+
+    headers.forEach((header,key) =>header.addEventListener("mouseover",()=>{
+        const child = header.lastElementChild; /* cach tam thoi */
+        child.style.display = "block"
+        overlayAppear();
+    }));
+    
+    headers.forEach((header,key) =>header.addEventListener("mouseout",()=>{
+        const child = header.lastElementChild;
+        child.style.display = null;
+        overlayDisappear();
+    }));
 
     modalContainers.forEach(modalContainer => modalContainer.addEventListener("mouseover", () =>{
-        overlay.style.display="block";
-        overlay.style.opacity="0.3";
+        overlayAppear();
     }));
   
-    contentTitles.forEach(contentTitle => contentTitle.addEventListener("mouseover", () =>{
-        overlay.style.display="block";
-        overlay.style.opacity="0.3";
-    }));
-  
-    modalContainers.forEach(modalContainer => modalContainer.addEventListener("mouseout",()=>{
-        overlay.style.display="none";
-        overlay.style.opacity="0";
+    modalContainers.forEach(modalContainer => modalContainer.addEventListener("mouseout",()=>{     
+        overlayDisappear();
     }));
 
-    contentTitles.forEach(contentTitle => contentTitle.addEventListener("mouseout",()=>{
+
+    const overlayAppear = function () {
+        const overlay = document.querySelector(".overlay");
+        overlay.style.display=" block";
+        overlay.style.opacity=" 0.3"; 
+    }
+
+    const overlayDisappear = function () {
+        const overlay = document.querySelector(".overlay");
         overlay.style.display="none";
-        overlay.style.opacity="0";
-    }));
+        overlay.style.opacity="0"; 
+    }
+
+/* hide sub header */
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".header__wrapper").style.top="0";
+        }else {
+            document.querySelector(".header__wrapper").style.top="-46px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+
 }
